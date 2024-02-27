f4f8b39209b52917aff0e52a8baf0a768658db91
BastienMartinelli <contact.bmartinelli@gmail.com>
2024-02-27 12:11:09 +0100

    Fixes TS issue

--- components/PageContent.tsx
+++ components/PageContent.tsx
@@ -1,29 +1,25 @@
 import { urlForImage } from "@/sanity/lib/image";
 import { BlockContent } from "@/sanity/types";
-import { PortableText, PortableTextProps } from "@portabletext/react";
+import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 
-export const PageContent: React.FC<
-  { children?: BlockContent } & Omit<PortableTextProps, "value">
-> = ({ children, components = {} }) => {
+export const PageContent: React.FC<{ children?: BlockContent }> = ({
+  children,
+}) => {
   return (
     !!children && (
       <PortableText
         value={children}
         components={{
-          ...components,
           list: {
-            ...components?.list,
             bullet: ({ children }) => (
               <ul className="list-disc ml-10">{children}</ul>
             ),
           },
           listItem: {
-            ...components?.listItem,
             bullet: ({ children }) => <li>{children}</li>,
           },
           types: {
-            ...components?.types,
             image: ({ value }) => (
               <div className="flex justify-center pt-8 pb-8">
                 <Image
