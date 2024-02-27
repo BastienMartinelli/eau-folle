02ad877c6394f8d9bc5a0ef5b8491976e9aa253b
BastienMartinelli <contact.bmartinelli@gmail.com>
2024-02-27 11:56:13 +0100

    Plugs home page to sanity

--- components/PageContent.tsx
+++ components/PageContent.tsx
@@ -1,25 +1,29 @@
 import { urlForImage } from "@/sanity/lib/image";
 import { BlockContent } from "@/sanity/types";
-import { PortableText } from "@portabletext/react";
+import { PortableText, PortableTextProps } from "@portabletext/react";
 import Image from "next/image";
 
-export const PageContent: React.FC<{ children?: BlockContent }> = ({
-  children,
-}) => {
+export const PageContent: React.FC<
+  { children?: BlockContent } & Omit<PortableTextProps, "value">
+> = ({ children, components = {} }) => {
   return (
     !!children && (
       <PortableText
         value={children}
         components={{
+          ...components,
           list: {
+            ...components?.list,
             bullet: ({ children }) => (
               <ul className="list-disc ml-10">{children}</ul>
             ),
           },
           listItem: {
+            ...components?.listItem,
             bullet: ({ children }) => <li>{children}</li>,
           },
           types: {
+            ...components?.types,
             image: ({ value }) => (
               <div className="flex justify-center pt-8 pb-8">
                 <Image
